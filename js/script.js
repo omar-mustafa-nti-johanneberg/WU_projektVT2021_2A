e749c8ea59b132c32dc7e9d19aff3813ca80615e
omar-mustafa-nti-johanneberg <omar.mustafa@elev.ga.ntig.se>
2021-02-26 16:43:42 +0100

    CSS och javascript funktion

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const lines = document.querySelector(".lines")
+const nav = document.querySelector(".nav-links")
+
+function toggleMeny() {
+    nav.classList.toggle("nav-active")
+    lines.classList.toggle("toggle")
+}
+
+lines.addEventListener('click', toggleMeny)
